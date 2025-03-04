91a02d4d89b4c444edb9d8741796c782bf37ba7f
Andy Attebery <me@andyattebery.com>
2025-03-03 22:40:08 -0600

    added support for specifying device

--- copy_bios_rom_files.bash
+++ copy_bios_rom_files.bash
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+shopt -s nocasematch
+
 # Directory Paths
 SOURCE_BIOS_DIR="nas-01:/mnt/storage/Games/System Files"
-SOURCE_ROMS_DIR="nas-01:/mnt/storage/Games/ROMs"
+SOURCE_ROMS_DIR="nas-01:/mnt/storage/Games/ROMs/Curated"
 
 EMUDECK_ROOT_DIR="/run/media/SDCARDNAME/"
-ESDE_BIOS_DIR="/Volumes/Untitled/BIOS"
-ESDE_ROMS_DIR="/Volumes/Untitled/ES-DE ROMs"
-MUOS_ROOT_DIR="/Volumes/SD2"
+# ESDE_BIOS_DIR="/Volumes/Android_Emu/BIOS"
+# ESDE_ROMS_DIR="/Volumes/Android_Emu/ROMs"
+ESDE_BIOS_DIR="trimui-brick:/userdata/bios"
+ESDE_ROMS_DIR="trimui-brick:/userdata/roms"
+MUOS_ROOT_DIR="/Volumes/MUOS_SD2"
 MINUI_ROOT_DIR="/Volumes/MINUI"
 SPRUCE_ROOT_DIR="/Volumes/SPRUCE"
 
@@ -20,6 +24,8 @@ readonly ATARI_JAGUAR="ATARI_JAGUAR"
 readonly ATARI_LYNX="ATARI_LYNX"
 readonly CBS_COLECOVISION="CBS_COLECOVISION"
 readonly COMMODORE_64="COMMODORE_64"
+readonly MAME="MAME"
+readonly MAME2003PLUS="MAME2003PLUS"
 readonly MICROSOFT_XBOX_360="MICROSOFT_XBOX_360"
 readonly MICROSOFT_XBOX="MICROSOFT_XBOX"
 readonly NEC_SUPERGRAFX="NEC_SUPERGRAFX"
@@ -59,6 +65,150 @@ readonly SONY_PLAYSTATION_3="SONY_PLAYSTATION_3"
 readonly SONY_PLAYSTATION_PORTABLE="SONY_PLAYSTATION_PORTABLE"
 readonly SONY_PLAYSTATION_VITA="SONY_PLAYSTATION_VITA"
 readonly SONY_PLAYSTATION="SONY_PLAYSTATION"
+
+# Systems
+declare -ra ANBERNIC_RGXX_SYSTEMS=(
+  $NINTENDO_GAME_BOY
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $SEGA_32X
+  $SEGA_CD
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  # $SONY_PLAYSTATION
+)
+
+declare -ra ANBERNIC_RGXX_ANALOGUE_STICKS_SYSTEMS=(
+  $NINTENDO_GAME_BOY
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $SEGA_32X
+  $SEGA_CD
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  $SONY_PLAYSTATION
+)
+
+declare -ra MIYOO_A30_SYSTEMS=(
+  # $NEC_TURBOGRAFX_16
+  $NINTENDO_GAME_BOY
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  # $NINTENDO_POKEMON_MINI
+  # $NINTENDO_SUPER_GAME_BOY
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  # $PICO_8
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  # $SEGA_MASTER_SYSTEM
+  # $SNK_NEO_GEO_POCKET
+  $SONY_PLAYSTATION
+)
+
+declare -a RAZER_EDGE_SYSTEMS=(
+  $NINTENDO_64
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_GAME_BOY
+  $NINTENDO_GAMECUBE
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_WII
+  $SEGA_32X
+  $SEGA_CD
+  $SEGA_DREAMCAST
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  $SEGA_MASTER_SYSTEM
+  $SEGA_SG_1000
+  $SONY_PLAYSTATION
+)
+
+declare -a RETROID_POCKET_5_SYSTEMS=(
+  $NINTENDO_64
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_GAME_BOY
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $SEGA_32X
+  $SEGA_CD
+  $SEGA_DREAMCAST
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  $SEGA_MASTER_SYSTEM
+  $SEGA_SG_1000
+  $SONY_PLAYSTATION
+)
+
+declare -a STEAM_DECK_SYSTEMS=(
+  $NINTENDO_64
+  $NINTENDO_FAMICOM_DISK_SYSTEM
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_GAME_BOY
+  $NINTENDO_GAMECUBE
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SWITCH
+  $NINTENDO_WII
+  $NINTENDO_WIIU
+  $SEGA_32X
+  $SEGA_CD
+  $SEGA_DREAMCAST
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  $SEGA_MASTER_SYSTEM
+  $SEGA_SG_1000
+  $SONY_PLAYSTATION
+)
+
+declare -ra TRIMUI_BRICK_SYSTEMS=(
+  $NINTENDO_GAME_BOY
+  $NINTENDO_GAME_BOY_ADVANCE
+  $NINTENDO_GAME_BOY_COLOR
+  $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
+  $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $SEGA_32X
+  $SEGA_CD
+  $SEGA_GAME_GEAR
+  $SEGA_GENESIS
+  # $SONY_PLAYSTATION
+)
+
+device_name_to_systems_array() {
+  case "$1" in
+  "anbernic_rgxx" | "rgxx" | "rg35xxsp")
+    echo "ANBERNIC_RGXX_SYSTEMS"
+    ;;
+  "anbernic_rgxx_analog_sticks" | "rgxx_analog_sticks" | "rg35xxh" | "rgcubexx")
+    echo "ANBERNIC_RGXX_ANALOGUE_STICKS_SYSTEMS"
+    ;;
+  "miyoo_a30" | "a30")
+    echo "MIYOO_A30_SYSTEMS"
+    ;;
+  "razer_edge")
+    echo "RAZER_EDGE_SYSTEMS"
+    ;;
+  "retroid_pocket_5")
+    echo "RETROID_POCKET_5_SYSTEMS"
+    ;;
+  "steam_deck")
+    echo "STEAM_DECK_SYSTEMS"
+    ;;
+  "trimui_brick" | "trimuibrick")
+    echo "TRIMUI_BRICK_SYSTEMS"
+    ;;
+  *)
+    echo "$1 is not a supported system."
+    ;;
+esac
+}
 
 # Source File Mappings
 declare -rA SYSTEM_TO_BIOS_FILES_GLOB_MAP=(
@@ -124,7 +274,7 @@ copy_bios_files() {
       local source_system_bios_files_glob=(${SYSTEM_TO_BIOS_FILES_GLOB_MAP[$system]})
       local destination_system_bios_directory=$(eval "$get_destination_system_bios_directory_function_name" $system)
       echo "rsync -avP \"$SOURCE_BIOS_DIR/$source_system_bios_files_glob\" \"$destination_system_bios_directory/\""
-      #rsync -avP "$SOURCE_BIOS_DIR/$source_system_bios_files_glob" "$destination_system_bios_directory/"
+      rsync -avP "$SOURCE_BIOS_DIR/$source_system_bios_files_glob" "$destination_system_bios_directory/"
     fi
 
   done
@@ -158,7 +308,7 @@ copy_rom_files() {
 }
 
 # EmuDeck (https://emudeck.github.io)
-declare -a ENABLED_EMUDECK_SYSTEMS=(
+declare -a DEFAULT_EMUDECK_SYSTEMS=(
   $NINTENDO_64
   $NINTENDO_FAMICOM_DISK_SYSTEM
   $NINTENDO_GAME_BOY_ADVANCE
@@ -238,12 +388,14 @@ get_system_emudeck_roms_directory(){
 }
 
 copy_to_emudeck() {
-  copy_bios_files ENABLED_EMUDECK_SYSTEMS "get_system_emudeck_bios_directory"
-  copy_rom_files ENABLED_EMUDECK_SYSTEMS "get_system_emudeck_roms_directory" false
+  local enabled_systems_array_name=${1:-DEFAULT_EMUDECK_SYSTEMS}
+
+  copy_bios_files $enabled_systems_array_name "get_system_emudeck_bios_directory"
+  copy_rom_files $enabled_systems_array_name "get_system_emudeck_roms_directory" false
 }
 
 # ES-DE (EmulationStation Desktop Edition) (https://www.es-de.org)
-declare -a ENABLED_ESDE_SYSTEMS=(
+declare -a DEFAULT_ESDE_SYSTEMS=(
   $NINTENDO_64
   $NINTENDO_FAMICOM_DISK_SYSTEM
   $NINTENDO_GAME_BOY_ADVANCE
@@ -323,12 +475,13 @@ get_system_esde_roms_directory(){
 }
 
 copy_to_esde() {
-  copy_bios_files ENABLED_ESDE_SYSTEMS "get_system_esde_bios_directory"
-  copy_rom_files ENABLED_ESDE_SYSTEMS "get_system_esde_roms_directory" false
+  local enabled_systems_array_name=${1:-DEFAULT_ESDE_SYSTEMS}
+  copy_bios_files $enabled_systems_array_name "get_system_esde_bios_directory"
+  copy_rom_files $enabled_systems_array_name "get_system_esde_roms_directory" false
 }
 
 # MinUI (https://github.com/shauninman/MinUI)
-declare -ra ENABLED_MINUI_SYSTEMS=(
+declare -ra DEFAULT_MINUI_SYSTEMS=(
   # Base
   $NINTENDO_GAME_BOY
   $NINTENDO_GAME_BOY_ADVANCE
@@ -392,20 +545,24 @@ get_system_minui_roms_directory(){
 }
 
 copy_to_minui() {
-  copy_bios_files ENABLED_MINUI_SYSTEMS "get_system_minui_bios_directory"
-  copy_rom_files ENABLED_MINUI_SYSTEMS "get_system_minui_roms_directory" false
+  local enabled_systems_array_name=${1:-DEFAULT_MINUI_SYSTEMS}
+
+  copy_bios_files $enabled_systems_array_name "get_system_minui_bios_directory"
+  copy_rom_files $enabled_systems_array_name "get_system_minui_roms_directory" false
 }
 
 # MuOS (https://muos.dev)
-declare -ra ENABLED_MUOS_SYSTEMS=(
+declare -ra DEFAULT_MUOS_SYSTEMS=(
   $NINTENDO_GAME_BOY
   $NINTENDO_GAME_BOY_ADVANCE
   $NINTENDO_GAME_BOY_COLOR
   $NINTENDO_NINTENDO_ENTERTAINMENT_SYSTEM
   $NINTENDO_SUPER_NINTENDO_ENTERTAINMENT_SYSTEM
+  $SEGA_32X
+  $SEGA_CD
   $SEGA_GAME_GEAR
   $SEGA_GENESIS
-  $SONY_PLAYSTATION
+  # $SONY_PLAYSTATION
 )
 
 get_system_muos_bios_directory(){
@@ -417,12 +574,14 @@ get_system_muos_roms_directory(){
 }
 
 copy_to_muos() {
-  copy_bios_files ENABLED_MUOS_SYSTEMS "get_system_muos_bios_directory"
-  copy_rom_files ENABLED_MUOS_SYSTEMS "get_system_muos_roms_directory" true
+  local enabled_systems_array_name=${1:-DEFAULT_MUOS_SYSTEMS}
+
+  copy_bios_files $enabled_systems_array_name "get_system_muos_bios_directory"
+  copy_rom_files $enabled_systems_array_name "get_system_muos_roms_directory" true
 }
 
 # Spruce (https://spruceui.github.io/)
-declare -ra ENABLED_SPRUCE_SYSTEMS=(
+declare -ra DEFAULT_SPRUCE_SYSTEMS=(
   # $NEC_TURBOGRAFX_16
   $NINTENDO_GAME_BOY
   $NINTENDO_GAME_BOY_ADVANCE
@@ -451,7 +610,7 @@ declare -rA SYSTEM_TO_SPRUCE_ROMS_SUBDIR_MAP=(
   [$ATARI_LYNX]='LYNX'
   [$CBS_COLECOVISION]='COLECO'
   [$COMMODORE_64]='COMMODORE'
-  [$MAME]='MAME2003PLUS'
+  [$MAME2003PLUS]='MAME2003PLUS'
   [$NEC_SUPERGRAFX]='SGFX'
   [$NEC_TURBOGRAFX_16]='PCE'
   [$NEC_TURBOGRAFX_CD]='PCECD'
@@ -490,28 +649,36 @@ get_system_spruce_roms_directory(){
 }
 
 copy_to_spruce() {
-  copy_bios_files ENABLED_SPRUCE_SYSTEMS "get_system_spruce_bios_directory"
-  copy_rom_files ENABLED_SPRUCE_SYSTEMS "get_system_spruce_roms_directory" false
+  local enabled_systems_array_name=${1:-DEFAULT_SPRUCE_SYSTEMS}
+
+  copy_bios_files "$enabled_systems_array_name" "get_system_spruce_bios_directory"
+  copy_rom_files "$enabled_systems_array_name" "get_system_spruce_roms_directory" false
 }
 
 # Main
 destination_type=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 
+systems_array_name=""
+if [ -z $2 ]; then
+  device=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+  systems_array_name=$(device_name_to_systems_array $device)
+fi
+
 case "$destination_type" in
   "emudeck")
-    copy_to_emudeck
+    copy_to_emudeck "$systems_array_name"
     ;;
   "esde")
-    copy_to_esde
+    copy_to_esde "$systems_array_name"
     ;;
   "minui")
-    copy_to_minui
+    copy_to_minui "$systems_array_name"
     ;;
   "muos")
-    copy_to_muos
+    copy_to_muos "$systems_array_name"
     ;;
   "spruce")
-    copy_to_spruce
+    copy_to_spruce "$systems_array_name"
     ;;
   *)
     echo "$1 is not a supported destination OS/application."
